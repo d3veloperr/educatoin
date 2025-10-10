08955966ed97d1b92d36a2b85fe1772d42109e69
d3veloperr <igor.avh@internet.ru>
2025-10-10 17:34:25 +0300

    Create num4.cpp
    create a file with code for task №4

---
+++ num4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+void printDoubleBits(double num) {
+   
+    union {
+        double source;        // само число
+        unsigned long long bits;
+    } data;
+    
+    data.source = num; 
+    
+    bitset<64> binary(data.bits);
+
+    cout << "Знаковый бит: " << binary[63] << endl;
+    cout << "Порядок (11 бит): ";
+    for(int i = 52; i <= 62; i++) {
+        cout << binary[i];
+    }
+    cout << endl;
+    cout << "Мантисса (52 бита): ";
+    for(int i = 0; i <= 51; i++) {
+        cout << binary[i];
+    }
+    cout << endl;
+}
+
+int main() {
+    double number = 7; 
+    cout << "Двоичное представление числа " << number << ":" << endl;
+    printDoubleBits(number);
+    return 0;
+}
