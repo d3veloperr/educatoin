e8217e99bc88dc77c4eb8b69eb061ccf9580a872
d3veloperr <igor.avh@internet.ru>
2025-11-29 02:51:36 +0300

    Update num32.cpp

--- num32.cpp
+++ num32.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 
 unsigned int changeBit(unsigned int number, int position, bool value) {
-   
     unsigned int mask = 1 << position;
-    
     if (value) {
         return number | mask;
     } else {
@@ -16,13 +14,10 @@ int main() {
     unsigned int number;
     int position;
     bool value;
-    
     cout << "Введите число: ";
     cin >> number;
-    
     cout << "Введите позицию бита (0-31): ";
     cin >> position;
-    
     cout << "Введите значение (0 или 1): ";
     cin;
 }
