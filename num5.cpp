1263ba3ea7c5750acd3d1a54463a6c1647e00d24
d3veloperr <igor.avh@internet.ru>
2025-11-29 14:25:18 +0300

    Update num5.cpp

--- num5.cpp
+++ num5.cpp
@@ -1,29 +1,35 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-setlocale(0, "");
+
 unsigned int changeBit(unsigned int number, int position, bool value) {
-unsigned int mask = 1 << position;
+    unsigned int mask = 1 << position;
     if (value) {
         return number | mask;
-    } 
-    else {
+    } else {
         return number & ~mask;
     }
 }
+
 int main() {
+    setlocale(0, "");
+
     unsigned int number;
     int position;
     bool value;
+
     cout << "Введите число: ";
     cin >> number;
+
     cout << "Введите позицию бита (0-31): ";
     cin >> position;
+
     cout << "Введите значение (0 или 1): ";
     cin >> value;
-    int result = changeBit(number, position, value);
+
+    unsigned int result = changeBit(number, position, value);
+
     cout << "Исходное число: " << number << endl;
     cout << "Результат: " << result << endl;
+
     return 0;
 }
